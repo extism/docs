f077f1d643fc05ab549dfba485078716eeb22671
Steve Manuel <nilslice@gmail.com>
2022-08-30 12:17:06 -0600

    chore: add more info to html title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Extism',
+  title: 'Extism - make all software programmable. Extend from within.',
   tagline: 'The Universal Plug-in System',
   url: 'https://extism.org',
   baseUrl: '/',
