58483eca849c921e64f433447107ec3c698178b2
Steve Manuel <steve@dylib.so>
2022-11-30 13:11:22 -0600

    docs: add announcement blog

--- src/components/PDKLogoLinks.jsx
+++ src/components/PDKLogoLinks.jsx
@@ -4,6 +4,7 @@ export default function PDKLogoLinks() {
     const sdks = [
         { name: "Rust", path: "/docs/write-a-plugin/rust-pdk", logo: "/img/sdk-languages/rust.svg" },
         { name: "Go", path: "/docs/write-a-plugin/go-pdk", logo: "/img/sdk-languages/go.svg" },
+        { name: "Haskell", path: "/docs/write-a-plugin/haskell-pdk", logo: "/img/sdk-languages/haskell.svg" },
         { name: "AssemblyScript", path: "/docs/write-a-plugin/assemblyscript-pdk", logo: "/img/sdk-languages/assemblyscript.svg" },
         { name: "C", path: "/docs/write-a-plugin/c-pdk", logo: "/img/sdk-languages/c.svg" },
     ]
