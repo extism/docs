afedd8ef509b15a8e581f2b8d3b04361901e7c32
Steve Manuel <steve@dylib.so>
2023-01-28 15:16:57 -0700

    feat: add js pdk logo and update name of browser sdk logo

--- src/components/SDKLogoLinks.jsx
+++ src/components/SDKLogoLinks.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function SDKLogoLinks({only}) {
     let sdks = [
-        { name: "Browser", path: "/docs/integrate-into-your-codebase/browser-runtime-sdk", logo: "/img/sdk-languages/js.svg" },
+        { name: "Browser", path: "/docs/integrate-into-your-codebase/browser-runtime-sdk", logo: "/img/sdk-languages/browser.svg" },
         { name: "C", path: "/docs/integrate-into-your-codebase/c-host-sdk", logo: "/img/sdk-languages/c.svg" },
         { name: "C++", path: "/docs/integrate-into-your-codebase/cpp-host-sdk", logo: "/img/sdk-languages/cpp.svg" },
         { name: "Elixir", path: "/docs/integrate-into-your-codebase/elixir-or-erlang-host-sdk", logo: "/img/sdk-languages/elixir.svg" },
