032c12f69d1e387d463d4844e9804fe5f7885e38
Steve Manuel <steve@dylib.so>
2024-01-09 07:52:29 -0700

    feat: add rotating language slots on homepage (#141)

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import SDKLogoLinks from '@site/src/components/SDKLogoLinks';
+import SDKLogoLinks, {SDKLogoRotator} from '@site/src/components/SDKLogoLinks';
+import { PDKLogoRotator } from '../components/PDKLogoLinks';
 
 import styles from './index.module.css';
 
@@ -13,9 +14,9 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--secondary', styles.heroBanner)}>
       <div className="container">
-        <p>
-          <img src='/img/extism-badge.svg' alt={siteConfig.title} />
-        </p>
+        <div className="cross-languages">
+          <span>Call</span> <PDKLogoRotator/> <span>code from your</span> <SDKLogoRotator/> <span>apps.</span>
+        </div>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
